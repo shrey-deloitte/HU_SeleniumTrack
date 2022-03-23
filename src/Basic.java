4bf277a3535997249f53c0e2e3b99bfa52442d39
shrey-deloitte <shredeshpande@deloitte.com>
2022-03-23 16:12:16 +0530

    mini assignment 2 initial push

---
+++ src/Basic.java
@@ -0,0 +1,54 @@
+import static java.lang.Thread.sleep;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basic {
+
+    public static void main(String[] args) throws InterruptedException {
+        String title="PHPTRAVELS";
+        String title2;
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\shredeshpande\\Downloads\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get(" https://phptravels.com/demo");
+
+        String parentwindow=driver.getWindowHandle();
+        String a=driver.getTitle();
+        if(a.equals(title)){
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+
+        driver.findElement(By.xpath("//a[text()='Login']")).click();
+        title2= driver.getTitle();
+
+        if(title2.equals(title)){
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+
+        sleep(9000);
+
+        driver.switchTo().window(parentwindow);
+        sleep(2000);
+
+        driver.findElement(By.xpath("//a[text()='Pricing']")).click();
+        sleep(3000);
+
+        driver.navigate().back();
+        sleep(3000);
+        driver.navigate().refresh();
+
+        driver.quit();
+
+    }
+
+}
